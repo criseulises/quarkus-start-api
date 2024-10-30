0b1415df5b8037e52a1b67fe6618b3c987684eda
Cristian Sanchez <cristiansanchez@MacBook-Air-de-User.local>
2024-10-30 10:56:40 -0400

    first commit

---
+++ src/main/java/quarkus/EcoResource.java
@@ -0,0 +1,13 @@
+package quarkus;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/greet")
+public class EcoResource {
+
+    @GET
+    public String greet() {
+        return "Hello Eco!";
+    }
+}
