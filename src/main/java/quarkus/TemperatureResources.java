2d4aa219d4388eb4a55db0b60a90f5df6d37c0c6
Cristian Sanchez <scristianeulises@gmail.com>
2024-11-04 18:39:14 -0400

    GET and POSTasdfasdf

--- src/main/java/quarkus/TemperatureResources.java
+++ src/main/java/quarkus/TemperatureResources.java
@@ -1,26 +1,30 @@
 package quarkus;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Path("temperatures")
 public class TemperatureResources {
 
+    private List<Temperature> temperatures = new ArrayList<>();
+
+    @POST
+    public List<Temperature> addTemperature(Temperature temperature) {
+        temperatures.add(temperature);
+        return temperatures;
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Temperature> list(){
-        return Arrays.asList(
-                new Temperature("Málaga",15,34),
-                new Temperature("RD",24,32),
-                new Temperature("Madrid", 32,43),
-                new Temperature("Mexico", 34,23)
-        );
+        return temperatures;
     }
 
     @GET
